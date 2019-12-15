da010a6e9e72618247dd3b6fb3d98532332e260e
Mbosc12 <47382848+Mbosc12@users.noreply.github.com>
2019-12-15 15:00:11 +0100

    Ajout fonctionnel du Panier

---
+++ src/main/webapp/js/Panier.js
@@ -0,0 +1,50 @@
+function LignePanier (code, qte, prix)
+{
+    this.codeArticle = code;
+    this.qteArticle = qte;
+    this.prixArticle = prix;
+    
+    this.ajouterQte = function(qte)
+    {
+        this.qteArticle += qte;
+    }
+    
+    this.getPrixLigne = function()
+    {
+        var resultat = this.prixArticle * this.qteArticle;
+        return resultat;
+    }
+    this.getCode = function() 
+    {
+        return this.codeArticle;
+    }
+}
+
+function Panier()
+{
+    this.liste = [];
+    this.ajouterArticle = function(code, qte, prix)
+    { 
+        var index = this.getArticle(code);
+        if (index == -1) this.liste.push(new LignePanier(code, qte, prix));
+        else this.liste[index].ajouterQte(qte);
+    }
+    this.getPrixPanier = function()
+    {
+        var total = 0;
+        for(var i = 0 ; i < this.liste.length ; i++)
+            total += this.liste[i].getPrixLigne();
+        return total;
+    }
+    this.getArticle = function(code)
+    {
+        for(var i = 0 ; i <this.liste.length ; i++)
+            if (code == this.liste[i].getCode()) return i;
+        return -1;
+    }
+    this.supprimerArticle = function(code)
+    {
+        var index = this.getArticle(code);
+        if (index > -1) this.liste.splice(index, 1);
+    }
+}
